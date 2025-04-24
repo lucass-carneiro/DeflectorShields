d1b43c4b48babab0828fdd1d54bfa74a21a9be5b
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-04-24 00:49:26 -0500

    Updated rust impl

--- src/evolve.rs
+++ src/evolve.rs
@@ -1,115 +1,19 @@
-use crate::errors::NormalizationError;
-use crate::metrics::Metric;
-use crate::params::{Normalization, SingleParams};
+use crate::alcubierre::AlcubierreData;
+use crate::types::ParticleState;
 
-use nalgebra::Vector4;
+pub fn rk4_step(h: f64, alcubierre_data: &AlcubierreData, state: &mut ParticleState<f64>) {
+    let y0 = state.clone();
 
-#[derive(Debug)]
-pub struct StateVector {
-    pub p: Vector4<f64>,
-    pub x: Vector4<f64>,
-}
+    let k1 = alcubierre_data.rhs(state);
+    *state = y0 + h * k1 / 2.0;
 
-pub fn normalize<T: Metric>(
-    params: SingleParams,
-    metric: &T,
-) -> Result<StateVector, NormalizationError> {
-    let norm = match params.normalize_as {
-        Normalization::Massive => -1.0,
-        Normalization::Photon => 0.0,
-    };
+    let k2 = alcubierre_data.rhs(state);
+    *state = y0 + h * k2 / 2.0;
 
-    let x = Vector4::new(
-        0.0,
-        params.particle.x1,
-        params.particle.x2,
-        params.particle.x3,
-    );
+    let k3 = alcubierre_data.rhs(state);
+    *state = y0 + h * k3;
 
-    let guu = metric.g_uu(&x);
+    let k4 = alcubierre_data.rhs(state);
 
-    let mut p = Vector4::new(
-        0.0,
-        params.particle.p1,
-        params.particle.p2,
-        params.particle.p3,
-    );
-
-    let a = guu[(0, 0)];
-
-    let b = 2.0 * (guu[(0, 1)] * p[1] + guu[(0, 2)] * p[2] + guu[(0, 3)] * p[3]);
-
-    let c = guu[(1, 1)] * p[1] * p[1]
-        + 2.0 * guu[(1, 2)] * p[1] * p[2]
-        + 2.0 * guu[(1, 3)] * p[1] * p[3]
-        + guu[(2, 2)] * p[2] * p[2]
-        + 2.0 * guu[(2, 3)] * p[2] * p[3]
-        + guu[(3, 3)] * p[3] * p[3]
-        - norm;
-
-    let delta = b * b - 4.0 * a * c;
-
-    if delta < 0.0 {
-        return Err(NormalizationError {
-            normalization: params.normalize_as,
-            p1: p[1],
-            p2: p[2],
-            p3: p[3],
-        });
-    }
-
-    let p0_a = (-b - f64::sqrt(delta)) / (2.0 * a);
-    let p0_b = (-b + f64::sqrt(delta)) / (2.0 * a);
-
-    if p0_a > 0.0 {
-        p[0] = p0_a;
-    } else if p0_b > 0.0 {
-        p[0] = p0_b;
-    }
-
-    Ok(StateVector { p, x })
-}
-
-pub fn rhs<T: Metric>(state: &StateVector, metric: &T) -> StateVector {
-    let ham_derivs = metric.hamiltonian_derivs(&state.p, &state.x);
-
-    let p_rhs = -Vector4::new(
-        ham_derivs[(0, 1)],
-        ham_derivs[(1, 1)],
-        ham_derivs[(2, 1)],
-        ham_derivs[(3, 1)],
-    );
-
-    let x_rhs = Vector4::new(
-        ham_derivs[(0, 0)],
-        ham_derivs[(1, 0)],
-        ham_derivs[(2, 0)],
-        ham_derivs[(3, 0)],
-    );
-
-    StateVector { p: p_rhs, x: x_rhs }
-}
-
-pub fn rk4_step<T: Metric>(state: &mut StateVector, metric: &T, h: f64) {
-    let y0 = StateVector {
-        p: state.p,
-        x: state.x,
-    };
-
-    let k1 = rhs(state, metric);
-    state.p = y0.p + h * k1.p / 2.0;
-    state.x = y0.x + h * k1.x / 2.0;
-
-    let k2 = rhs(state, metric);
-    state.p = y0.p + h * k2.p / 2.0;
-    state.x = y0.x + h * k2.x / 2.0;
-
-    let k3 = rhs(state, metric);
-    state.p = y0.p + h * k3.p;
-    state.x = y0.x + h * k3.x;
-
-    let k4 = rhs(state, metric);
-
-    state.p = y0.p + h * (k1.p / 6.0 + k2.p / 3.0 + k3.p / 3.0 + k4.p / 6.0);
-    state.x = y0.x + h * (k1.x / 6.0 + k2.x / 3.0 + k3.x / 3.0 + k4.x / 6.0);
+    *state = y0 + h * (k1 / 6.0 + k2 / 3.0 + k3 / 3.0 + k4 / 6.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,69 @@
+use evolve::rk4_step;
+
 mod alcubierre;
 mod errors;
 mod evolve;
-mod logging;
-mod metrics;
-mod minkowski;
 mod params;
 mod tsv;
+mod types;
+
+fn init_logger() {
+    use env_logger::{Builder, Env};
+    use std::io::Write;
+
+    let env = Env::default().filter_or("DS_LOG_LEVEL", "info");
+
+    Builder::from_env(env)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[Deflector Shield {} {}]: {}",
+                buf.timestamp(),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
+}
 
 fn main() {
-    logging::init_logger();
+    init_logger();
 
-    // Parse args
-    let cmd_args: Vec<String> = std::env::args().collect();
+    let par = params::read_params("params.json").unwrap();
 
-    if cmd_args.len() != 2 {
-        log::error!("Usage: {} <parameter-file>", cmd_args[0]);
-        return;
-    }
+    match par.single_particle_id {
+        Some(id) => {
+            log::info!("Single particle mode");
 
-    // Parse params
-    log::info!("Reading parameter file \"{}\"", cmd_args[1]);
+            let mut state = par
+                .alcubierre_data
+                .make_normalized_state(
+                    id.x0,
+                    id.y0,
+                    id.z0,
+                    id.px0,
+                    id.py0,
+                    id.pz0,
+                    par.normalize_as,
+                )
+                .unwrap();
 
-    let par = match params::read_params(&cmd_args[1]) {
-        Ok(o) => o,
-        Err(e) => {
-            log::error!("{}", e.to_string());
-            return;
+            let mut out_file = tsv::TSVFile::new("single.tsv");
+
+            let nlambda = (par.affine_data.lambda_max / par.affine_data.dlambda) as u64;
+
+            for i in 0..=nlambda {
+                let t = (i as f64) * par.affine_data.dlambda;
+                log::info!("Integrating step {}/{}, t = {}", i, nlambda, t);
+
+                rk4_step(par.affine_data.dlambda, &par.alcubierre_data, &mut state);
+
+                out_file.append(i, t, &state);
+            }
+
+            log::info!("Single particle mode finished");
         }
-    };
 
-    // Time step data
-    let tf = par.time_integration.tf;
-    let dt = par.time_integration.dt;
-    let nt = (f64::ceil(tf / dt) as u64) + 1;
-
-    // Output file
-    let mut output_file = tsv::make_file("out.tsv");
-
-    // Spacetime metric
-    let metric = alcubierre::Alcubierre::new(0.5, 0.5, 1.0);
-
-    // Normalize and evolve
-    log::info!("Normalizing initial p_0");
-    let mut state = evolve::normalize(par, &metric).unwrap();
-
-    log::info!("Normalized initial p_0 is {}", state.p[0]);
-
-    for i in 0..nt {
-        let t = (i as f64) * dt;
-
-        log::info!("Evolution step {}, t = {}", i, t);
-
-        tsv::append_data(&mut output_file, i, t, &state);
-        evolve::rk4_step(&mut state, &metric, dt);
+        None => log::info!("Multi particle mode not implemented yet"),
     }
 }
