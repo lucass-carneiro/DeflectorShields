9fbf8873ecc060f5f28a31a96b80e60a0deaf136
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-06-24 16:03:34 -0500

    Added acessor struct to particle state data

--- deflector-core/src/types.rs
+++ deflector-core/src/types.rs
@@ -6,3 +6,30 @@ pub enum ParticleType {
     Massive,
     Photon,
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct ParticleStateComponents<T> {
+    pub t: T,
+    pub x: T,
+    pub y: T,
+    pub z: T,
+    pub pt: T,
+    pub px: T,
+    pub py: T,
+    pub pz: T,
+}
+
+impl<T: Copy> ParticleStateComponents<T> {
+    pub fn from_state(state: &ParticleState<T>) -> ParticleStateComponents<T> {
+        ParticleStateComponents {
+            t: state[0],
+            x: state[0],
+            y: state[0],
+            z: state[0],
+            pt: state[0],
+            px: state[0],
+            py: state[0],
+            pz: state[0],
+        }
+    }
+}
